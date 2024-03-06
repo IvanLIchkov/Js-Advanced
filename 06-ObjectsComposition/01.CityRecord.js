0b44ebfbb9a38083a1c245768a867d9cb7239f33
Ivan Lichkov <Vanko.lichkov@icloud.com>
2024-03-06 10:12:46 +0200

    task 1 and word with tasks

---
+++ 06-ObjectsComposition/01.CityRecord.js
@@ -0,0 +1,12 @@
+function City(name, population, treasury){
+    return {
+        name: name,
+        population: population,
+        treasury: treasury
+    };
+}
+
+console.log(City('Tortuga',
+    7000,
+    15000
+));
