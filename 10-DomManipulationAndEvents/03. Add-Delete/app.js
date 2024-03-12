91b9534bd3ac10356dccb4eed82f7258d347371e
Ivan Lichkov <Vanko.lichkov@icloud.com>
2024-03-12 17:45:44 +0200

    Some tasks solutions.

--- 10-DomManipulationAndEvents/03. Add-Delete/app.js
+++ 10-DomManipulationAndEvents/03. Add-Delete/app.js
@@ -1,3 +1,13 @@
 function addItem() {
-    //TODO...
+    let liElement = document.createElement('li');
+    liElement.textContent = document.getElementById('newItemText').value;
+
+    let deleteBtn = document.createElement('a');
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.href = '#'
+    liElement.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener('click',() => liElement.remove())
+    document.getElementById('items').appendChild(liElement);
+
 }
