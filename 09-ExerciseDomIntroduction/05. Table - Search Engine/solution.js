71ae06c8364c6b4651cdfaa4d8d41f85826ce3c4
Ivan Lichkov <Vanko.lichkov@icloud.com>
2024-03-12 11:08:26 +0200

    Some task solutions.

--- 09-ExerciseDomIntroduction/05. Table - Search Engine/solution.js
+++ 09-ExerciseDomIntroduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,19 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+      const string = document.getElementById('searchField').value.toLowerCase();
+      const table = document.querySelector('tbody').querySelectorAll('tr');
+      const regex = `\\b\\w*${string}\\w*\\b`
+      for (const tr of table) {
+         tr.className = '';
+         let elements = tr.querySelectorAll('td');
+         for (const element of elements) {
+            const stringToMatch = element.textContent.toLowerCase();
+            if(stringToMatch.match(regex)){
+               tr.className = 'select';
+               break;
+            }
+         }
+      }
    }
 }
--- 09-ExerciseDomIntroduction/06. Format the Text/solution.js
+++ 09-ExerciseDomIntroduction/06. Format the Text/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  //TODO
+    const sentences = document.getElementById('input').value.split('.');
+    let length = sentences.length;
+    let output = '';
+    output = makeP(length);
+    document.getElementById('output').innerHTML = output;
+    function makeP(length) {
+        output +='<p>';
+        for (let i = 0; i <= length; i++) {
+            if (length === 1){
+                output+=sentences[0];
+                output+=`</p>`;
+                return output;
+            }
+            output+=sentences[0]+'.';
+            sentences.shift()
+            length--;
+            if (i===2){
+                break;
+            }
+        }
+        output+=`</p>\n`;
+        return makeP(length);
+    }
 }
