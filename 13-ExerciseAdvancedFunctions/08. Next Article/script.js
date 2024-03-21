a546000c74366ac0a1115e4645b0923449980a61
Ivan Lichkov <Vanko.lichkov@icloud.com>
2024-03-21 11:09:28 +0200

    Some tasks

--- 13-ExerciseAdvancedFunctions/08. Next Article/script.js
+++ 13-ExerciseAdvancedFunctions/08. Next Article/script.js
@@ -1,3 +1,16 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const arrOfArticles = articles;
+
+    const contentBox = document.getElementById('content');
+
+    function getArticleGenerator() {
+        const article = document.createElement('article');
+        if (arrOfArticles.length === 0){
+            return;
+        }
+        article.textContent = arrOfArticles.shift();
+        contentBox.appendChild(article);
+    }
+    return getArticleGenerator;
 }
+let test = getArticleGenerator;
